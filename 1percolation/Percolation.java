3b1eb2168508010f5b11e7c696abd5c223509884
Richard Bowen <richard@softwaregarage.com>
2020-12-12 22:22:41 -0500

    Submission 1

--- 1percolation/Percolation.java
+++ 1percolation/Percolation.java
@@ -4,28 +4,106 @@
  *  Last modified:     1/1/2019
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+
 public class Percolation {
 
+    private int mSize;
+    private final int mStartIdx;
+    private final int mEndIdx;
+    private WeightedQuickUnionUF qf;
+    private int mOpenSites = 0;
+    private boolean[] mSeen;
+
     // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n)
+    public Percolation(int n) {
+        mSize = n;
+        int size = n * n + 2;
+        mStartIdx = n * n;
+        mEndIdx = n * n + 1;
+        qf = new WeightedQuickUnionUF(size);
+//        for (int i = 1; i <= n; i++) {
+//            qf.union(0, i);
+//            qf.union(mEndIdx, mEndIdx + i);
+//        }
+        mSeen = new boolean[size];
+        for (int i = 0; i < size; i++)
+            mSeen[i] = false;
+    }
+
+    private int rci(int r, int c) {
+        return mSize * (r - 1) + (c - 1);
+    }
 
     // opens the site (row, col) if it is not open already
-    public void open(int row, int col)
+    public void open(int row, int col) {
+        if (row < 1 || row > mSize || col < 1 || col > mSize) throw new IllegalArgumentException();
+        int i = rci(row, col);
+        if (!mSeen[i])
+            mOpenSites += 1;
+        mSeen[i] = true;
+
+        int r2 = row - 1;
+        int c2 = col;
+        if (r2 > 0)
+            if (mSeen[rci(r2, c2)] && mSeen[rci(row, col)])
+                qf.union(rci(r2, c2), rci(row, col));
+
+        r2 = row;
+        c2 = col + 1;
+        if (c2 <= mSize)
+            if (mSeen[rci(r2, c2)] && mSeen[rci(row, col)])
+                qf.union(rci(r2, c2), rci(row, col));
+
+        r2 = row + 1;
+        c2 = col;
+        if (r2 <= mSize)
+            if (mSeen[rci(r2, c2)] && mSeen[rci(row, col)])
+                qf.union(rci(r2, c2), rci(row, col));
+
+        r2 = row;
+        c2 = col - 1;
+        if (c2 > 0)
+            if (mSeen[rci(r2, c2)] && mSeen[rci(row, col)])
+                qf.union(rci(r2, c2), rci(row, col));
+
+        if (row == 1)
+            qf.union(mStartIdx, rci(row, col));
+        if (row == mSize)
+            qf.union(mEndIdx, rci(row, col));
+
+        boolean test = percolates();
+        System.out.println(test);
+    }
 
     // is the site (row, col) open?
-    public boolean isOpen(int row, int col)
+    public boolean isOpen(int row, int col) {
+        if (row < 1 || row > mSize || col < 1 || col > mSize) throw new IllegalArgumentException();
+        return mSeen[rci(row, col)];
+    }
 
     // is the site (row, col) full?
-    public boolean isFull(int row, int col)
+    public boolean isFull(int row, int col) {
+        if (row < 1 || row > mSize || col < 1 || col > mSize) throw new IllegalArgumentException();
+        return qf.connected(mStartIdx,rci(row, col));
+    }
 
     // returns the number of open sites
-    public int numberOfOpenSites()
+    public int numberOfOpenSites() {
+        return mOpenSites;
+    }
 
     // does the system percolate?
-    public boolean percolates()
+    public boolean percolates() {
+        return qf.connected(mStartIdx, mEndIdx);
+    }
 
     // test client (optional)
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        Percolation perc = new Percolation(20);
+        perc.percolates();
+    }
 }
 
 
--- 1percolation/PercolationStats.java
+++ 1percolation/PercolationStats.java
@@ -7,21 +7,43 @@
 public class PercolationStats {
 
     // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials)
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) throw new IllegalArgumentException();
+
+
+    }
 
     // sample mean of percolation threshold
-    public double mean()
+    public double mean() {
+        return 0;
+    }
 
     // sample standard deviation of percolation threshold
-    public double stddev()
+    public double stddev() {
+        return 0;
+    }
 
     // low endpoint of 95% confidence interval
-    public double confidenceLo()
+    public double confidenceLo() {
+        return 0;
+    }
 
     // high endpoint of 95% confidence interval
-    public double confidenceHi()
+    public double confidenceHi() {
+        return 0;
+    }
 
     // test client (see below)
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        if (args.length<2)
+            throw new IllegalArgumentException("Specify two args");
+        int n = Integer.parseInt(args[0]);
+        int t = Integer.parseInt(args[1]);
+
+        for (int i=0; i<t; i++){
+            Percolation perc = new Percolation(t);
+
+        }
+    }
 
 }
