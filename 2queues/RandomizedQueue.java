ad2ce506935f6ecc42c70df621bd9174fa493cb5
Richard Bowen <richard@softwaregarage.com>
2020-12-27 22:50:55 -0500

    add RandomizedQueue iterator

--- 2queues/RandomizedQueue.java
+++ 2queues/RandomizedQueue.java
@@ -1,15 +1,50 @@
 import edu.princeton.cs.algs4.StdRandom;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
-
-    private final Deque<Item> deque = new Deque<>();
 
     // construct an empty randomized queue
     public RandomizedQueue() {
 
     }
+
+    private class RandomizedQueueIterator<ItemIt> implements Iterator<ItemIt> {
+
+        private RandomizedQueueIterator(Deque<ItemIt> dq) {
+            vals = (ItemIt[]) new Object[dq.size()];
+            int i = 0;
+            for (ItemIt it : dq)
+                vals[i++] = it;
+            StdRandom.shuffle(vals);
+        }
+
+        ItemIt[] vals;
+        int idx = 0;
+
+        @Override
+        public boolean hasNext() {
+            return idx < vals.length;
+        }
+
+        @Override
+        public ItemIt next() {
+            if (!hasNext())
+                throw new NoSuchElementException();
+
+            return vals[idx++];
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+
+    private final Deque<Item> deque = new Deque<>();
+
 
     // is the randomized queue empty?
     public boolean isEmpty() {
@@ -36,18 +71,23 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     // return a random item (but do not remove it)
     public Item sample() {
-        return deque.peekFirst();
+        return deque.iterator().next();
     }
 
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
-        return deque.iterator();
+        return new RandomizedQueueIterator<>(deque);
     }
 
     // unit testing (required)
     public static void main(String[] args) {
         RandomizedQueue<String> rq = new RandomizedQueue<>();
-        rq.enqueue("test");
+        rq.enqueue("aa");
+        rq.enqueue("bb");
+
+        for(String s : rq)
+            System.out.println(s);
+
         rq.dequeue();
     }
 
